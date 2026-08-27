8c98966ed62078b245232b3fa1e0d785c9389137
Marley <volpevox@outlook.fr>
2026-08-27 02:09:02 +0200

    Ajoute un panneau de diagnostic invisible (?debug=1) pour mesurer precisement le bug du menu du bas

--- artisan-devis/app/layout.tsx
+++ artisan-devis/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Poppins, Montserrat, Patrick_Hand, Roboto } from "next/font/google";
 import "./globals.css";
 import { BottomNav } from "@/components/BottomNav";
+import { DebugOverlay } from "@/components/DebugOverlay";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -59,6 +60,7 @@ export default function RootLayout({
       className={`${poppins.variable} ${montserrat.variable} ${patrickHand.variable} ${roboto.variable}`}
     >
       <body>
+        <DebugOverlay />
         <div className="app-viewport">
           <div className="app-scroll">{children}</div>
           <BottomNav />
