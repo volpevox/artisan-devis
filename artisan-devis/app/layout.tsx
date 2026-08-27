9013b9d1193c249938aea3e24b134fe301821ec0
Marley <volpevox@outlook.fr>
2026-08-27 11:13:09 +0200

    Revert "Tente une nouvelle correction du menu qui flotte au premier lancement iOS PWA"
    This reverts commit e8989db89d893b866b60063406b4d6c741dab6b3.

--- artisan-devis/app/layout.tsx
+++ artisan-devis/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Poppins, Montserrat, Patrick_Hand, Roboto } from "next/font/google";
 import "./globals.css";
 import { BottomNav } from "@/components/BottomNav";
-import { SyncViewportHeight } from "@/components/SyncViewportHeight";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -61,7 +60,6 @@ export default function RootLayout({
       className={`${poppins.variable} ${montserrat.variable} ${patrickHand.variable} ${roboto.variable}`}
     >
       <body>
-        <SyncViewportHeight />
         <div className="app-viewport">
           <div className="app-scroll">{children}</div>
           <BottomNav />
